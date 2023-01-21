b47c69f11e8eef5e6a341c83e38118a3d6e67189
mdatif796 <mdatif796@gmail.com>
2023-01-21 11:37:48 +0530

    setting up project.

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+
+function App() {
+  return (
+    <div id="ipod" style={styles}>
+      Ipod
+    </div>
+  );
+}
+
+const styles = {
+  width: 350,
+  height: 550,
+  borderRadius: 30,
+  backgroundColor: '#d8d8c4'
+}
+
+export default App;
